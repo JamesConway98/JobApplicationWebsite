f4be489daaa5a04f68592bab9fb9bf7c235a49ad
Munn Lok Eliot Soo SE2016 <munn.soo.2016@uni.strath.ac.uk>
2018-11-22 16:03:16 +0000

    Updated to include css and navbar

--- joblistings.php
+++ joblistings.php
@@ -1,43 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" type="text/css" href="mystyle.css">
 <head>
     <meta charset="UTF-8">
     <title>Job Listings</title>
-    <!--    <link rel="stylesheet" type="text/css" href="???.css">  -->
 </head>
+<header>
+    <h1>Jobs</h1>
+    <?php
+    require 'navbar.php'
+    ?>
+</header>
 <body>
-<h1>Jobs</h1>
 <?php
 //require 'navbar.php';
 require 'connectdb.php';
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
-    if(isset($_GET["page"]) && !($_GET["page"] < 1)){
+    if (isset($_GET["page"]) && !($_GET["page"] < 1)) {
         $page = $_GET["page"];
-    }else{
+    } else {
         $page = 1;
     }
     $page_size = 15;
-    $start_from = ($page-1) * $page_size;
+    $start_from = ($page - 1) * $page_size;
 
-    $sql = 'SELECT Job.ID, Job.Title, Job.Company, Job.Deadline, Job.Location, `occ`.`Occupation`, `typ`.`Type` FROM `Job` LEFT OUTER JOIN Occupation occ ON Job.Occupation_ID = occ.ID LEFT OUTER JOIN `Type` typ ON `Job`.`Type_ID` = `typ`.`ID` ORDER BY Deadline ASC, Title ASC LIMIT '.$start_from.', '.$page_size;
+    $sql = 'SELECT Job.ID, Job.Title, Job.Company, Job.Deadline, Job.Location, `occ`.`Occupation`, `typ`.`Type` FROM `Job` LEFT OUTER JOIN Occupation occ ON Job.Occupation_ID = occ.ID LEFT OUTER JOIN `Type` typ ON `Job`.`Type_ID` = `typ`.`ID` ORDER BY Deadline ASC, Title ASC LIMIT ' . $start_from . ', ' . $page_size;
     $jobs = $conn->query($sql);
     if (isset($jobs->num_rows) && $jobs->num_rows > 0) {
         echo '<table>';
         echo "<tr><th>Title</th><th>Company</th><th>Deadline</th><th>Location</th><th>Occupation</th><th>Type</th><th></th></tr>";
         while ($row = $jobs->fetch_assoc()) {
-            echo "<tr><td>" . $row["Title"] . "</td><td>" . $row["Company"] . "</td><td>" . $row["Deadline"] . "</td><td>" . $row["Location"] . "</td><td>" . $row["Occupation"] . "</td><td>" . $row["Type"] . "</td><td><form action='details.php' method='get'><button name='more' type='submit' value='".$row["ID"]."'>+</button> </form></td></tr>";
+            echo "<tr><td>" . $row["Title"] . "</td><td>" . $row["Company"] . "</td><td>" . $row["Deadline"] . "</td><td>" . $row["Location"] . "</td><td>" . $row["Occupation"] . "</td><td>" . $row["Type"] . "</td><td><form action='details.php' method='get'><button name='more' type='submit' value='" . $row["ID"] . "'>+</button> </form></td></tr>";
         }
         echo "</table>";
         echo "<div>";
-        if($page-1 > 0){
-            echo "<a href='joblistings.php?page=".($page-1)."'><</a>  ";
+        if ($page - 1 > 0) {
+            echo "<a href='joblistings.php?page=" . ($page - 1) . "'><</a>  ";
         }
         $sql = "SELECT COUNT(ID) as `count` FROM `Job`";
         $num_rows = $conn->query($sql)->fetch_assoc()["count"];
-        if($page+1 <= ceil($num_rows/$page_size)){
-            echo "<a href='joblistings.php?page=".($page+1)."'>></a>";
+        if ($page + 1 <= ceil($num_rows / $page_size)) {
+            echo "<a href='joblistings.php?page=" . ($page + 1) . "'>></a>";
         }
         echo "</div>";
     } else {
