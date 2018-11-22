e82dfc056b4130b2f5fd4bda4da4f3e98c798940
Munn Lok Eliot Soo SE2016 <munn.soo.2016@uni.strath.ac.uk>
2018-11-22 16:03:41 +0000

    Updated to include css and navbar

--- details.php
+++ details.php
@@ -1,45 +1,54 @@
 <?php
 require 'connectdb.php';
-if(!isset($_GET["more"]) || $conn->connect_error){
+if (!isset($_GET["more"]) || $conn->connect_error) {
     header("Location: joblistings.php");
     die();
 }
-$sql = 'SELECT * FROM `Job` LEFT OUTER JOIN Occupation occ ON Job.Occupation_ID = occ.ID LEFT OUTER JOIN `Type` typ ON `Job`.`Type_ID` = `typ`.`ID` WHERE ' . $_GET["more"] . '=`Job`.`ID`' ;
+$sql = 'SELECT * FROM `Job` LEFT OUTER JOIN Occupation occ ON Job.Occupation_ID = occ.ID LEFT OUTER JOIN `Type` typ ON `Job`.`Type_ID` = `typ`.`ID` WHERE ' . $_GET["more"] . '=`Job`.`ID`';
 $job = $conn->query($sql);
-if(isset($job->num_rows) && $job->num_rows > 0){
+if (isset($job->num_rows) && $job->num_rows > 0) {
     $job = $job->fetch_assoc();
-}else{
+} else {
     $job = null;
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" type="text/css" href="mystyle.css">
 <head>
     <meta charset="UTF-8">
-    <title><?php if(isset($job)){echo $job["Title"];}{echo "Job not found";}?></title>
-    <!--    <link rel="stylesheet" type="text/css" href="???.css">  -->
+    <title><?php if (isset($job)) {
+            echo $job["Title"];
+        } else {
+            echo "Job not found";
+        } ?></title>
 </head>
+<header>
+    <h1>Jobs</h1>
+    <?php
+    require 'navbar.php';
+    ?>
+</header>
 <body>
 <?php
-if(isset($job)){
-    echo '<div><div class="col">';
-    echo "<h1>".$job["Title"]."</h1>";
+if (isset($job)) {
+    echo '<div class="col">';
+    echo "<h2>" . $job["Title"] . "</h2>";
+    echo $job["Description"];
     echo '<form action="apply.php" method="get">'
-        . '<table align="left">'
-        . '<tr><td>Company:</td><td>'.$job["Company"].'</td></tr>'
-        . '<tr><td>Location:</td><td>'.$job["Location"].'</td></tr>'
-        . '<tr><td>Deadline:</td><td>'.$job["Deadline"].'</td></tr>'
-        . '<tr><td>Occupation:</td><td>'.$job["Occupation"].'</td></tr>'
-        . '<tr><td>Type:</td><td>'.$job["Type"].'</td></tr>'
+        . '<table>'
+        . '<tr><td>Company:</td><td>' . $job["Company"] . '</td></tr>'
+        . '<tr><td>Location:</td><td>' . $job["Location"] . '</td></tr>'
+        . '<tr><td>Deadline:</td><td>' . $job["Deadline"] . '</td></tr>'
+        . '<tr><td>Occupation:</td><td>' . $job["Occupation"] . '</td></tr>'
+        . '<tr><td>Type:</td><td>' . $job["Type"] . '</td></tr>'
         . '<tr><td colspan="2">'
-        . '<button name="apply" type="submit" align="left" value="' . $job["ID"] . '">Apply</button>'
+        . '<button name="apply" type="submit" align="center" value="' . $job["ID"] . '">Apply</button>'
         . '</td></tr>'
         . '</table>'
         . '</form>';
-    echo '</div><div align="center">';
-    echo $job["Description"];
-    echo '</div></div>';
-}else{
+    echo '</div>';
+} else {
     echo "<p>404 - Job not found.</p>";
 }
 ?>
