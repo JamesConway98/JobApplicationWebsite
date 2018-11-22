b013d290a5cdec2e56c1f63154ecb2be6609ad7d
Leonard Magyar <gcb16177@uni.strath.ac.uk>
2018-11-22 14:10:44 +0000

    apply and application confirmation page added

---
+++ apply.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: magyl
+ * Date: 22/11/2018
+ * Time: 14:08
+ */session_start();
+$_SESSION['occupation'] = "";
+$_SESSION['company'] = "";
+$_SESSION['location'] = "";
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <script>
+        function validateEmail(text) {
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(String(text).toLowerCase());
+        }
+
+        function validateForm() {
+            var name = document.forms["myform"]["name"].value;
+            var phone = document.forms["myform"]["phone"].value;
+            var email = document.forms["myform"]["email"].value;
+            var errs = "";
+
+            if((name == "" || name == null)){
+                errs += "   * Name must not be empty\n";
+
+            }
+
+            if ( (phone==null) || (phone =="")) {
+                errs += "   * Phone number must not be empty\n";
+
+            }
+
+            if (isNaN(phone)){
+                errs += "   * Phone number must be a number\n";
+            }
+            if(phone.length !=11){
+                errs += "   * Phone number must contain 11 numbers\n";
+
+            }
+            if(!validateEmail(email)){
+                errs += "   * Email must be valid format\n";
+            }
+
+
+            if (errs!=""){
+                alert("Sorry the following need corrected:\n"+errs);
+            }
+
+
+
+            return (errs=="");
+        }
+    </script>
+    <?php
+    $host = "devweb2018.cis.strath.ac.uk";
+    $user = "cs312groupt";
+    $pass = "soo7ZaiLaec8";
+    $dbname = "cs312groupt";
+    $conn = new mysqli($host, $user, $pass, $dbname);
+    if(!isset($_GET["apply"]) || $conn->connect_error){
+        header("Location: joblistings.php");
+        die();
+    }
+    $sql = 'SELECT * FROM `Job` LEFT OUTER JOIN Occupation occ ON Job.Occupation_ID = occ.ID LEFT OUTER JOIN `Type` typ ON `Job`.`Type_ID` = `typ`.`ID` WHERE ' . $_GET["apply"] . '=`Job`.`ID`';
+    $job = $conn->query($sql);
+    if(isset($job->num_rows) && $job->num_rows > 0){
+        $job = $job->fetch_assoc();
+        $_SESSION['occupation'] = $job["Occupation"];
+        $_SESSION['company'] = $job["Company"];
+        $_SESSION['location'] = $job["Location"];
+    }else{
+        $job = null;
+    }
+    ?>
+
+    <title><?php if(isset($job))
+        {
+            echo "Applying for ".$job["Title"];
+        }
+        else
+        {
+            echo "Job not found";
+        }
+        ?>
+    </title>
+</head>
+<body>
+
+<h2>Apply for a job</h2>
+
+<div>
+    <form action="insertApply.php" onsubmit="return validateForm()" method="post" name="myform" enctype="multipart/form-data">
+
+
+        <p><?php echo $job["Occupation"];?></p>
+        <p><?php echo $job["Company"];?></p>
+        <p><?php echo $job["Location"];?></p>
+        <p>
+            Name: <input type="text" name="name"/><br/>
+            Email: <input type="text" name="email"/><br/>
+            Phone number: <input type="text" name="phone"/><br/>
+            CV: <input type="hidden" name="MAX_FILE_SIZE" value="99999999" />
+            <input name="userfile" type="file"/>
+            <br>
+            Cover letter (optional):
+            <br>
+            <textarea name="message" rows="10" cols="30"></textarea>
+            <br>
+            <button name = "confirm" type="submit" value=<?php $_GET["apply"]?>>Continue</button>
+            <button type="button" onclick="history.back();">Back</button>
+        </p>
+
+    </form>
+</div>
+</body>
+</html>
---
+++ insertApply.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: magyl
+ * Date: 22/11/2018
+ * Time: 14:09
+ */session_start();
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Job application confirmation</title>
+
+</head>
+
+<h2>Application confirmation</h2>
+<?php
+function upload()
+{
+    if (is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+        /***  get the image info. ***/
+
+        $data = file_get_contents($_FILES['userfile']['tmp_name']);
+        $type = $_FILES['userfile']['type'];
+        $name = $_FILES['userfile']['name'];
+        $maxsize = 99999999;
+
+
+        if ($_FILES['userfile']['size'] < $maxsize) {
+            $host = "devweb2018.cis.strath.ac.uK";
+            $user = "cs312groupt";
+            $pass = "soo7ZaiLaec8";
+            $dbname = "cs312groupt";
+            $conn = new mysqli($host, $user, $pass, $dbname);
+
+
+            if ($conn->connect_error) {
+                die("Connection failed : " . $conn->connect_error);
+            }
+            $stmt = $conn->prepare("INSERT INTO Applications (name,email,phone,CV,message,status,company,occupation,location) VALUES (?,?,?,?,?,?,?,?,?)");
+
+
+            if (!$stmt->bind_param("sssbsssss", $name, $email, $phone, $data, $message, $status, $company, $occupation, $location)) {
+                die("Failed to bind parameter");
+            }//FIXME only show error during debugging*/
+
+            if (!$stmt->send_long_data(3, $data)) {
+                die("Failed to send long data");
+            }//FIXME only show error during debugging
+            $name = isset($_POST["name"]) ? $_POST["name"]: "";
+            $email = isset($_POST["email"]) ? $_POST["email"]: "";
+            $phone = isset($_POST["phone"]) ? $_POST["phone"]: "";
+            $message = isset($_POST["message"]) ? $_POST["message"]: "";
+            $status = "Application recieved";
+            $company = isset($_SESSION['company']) ? $_SESSION['company']: "";
+            $occupation = isset($_SESSION['occupation']) ? $_SESSION['occupation']: "";
+            $location = isset($_SESSION['location']) ? $_SESSION['location']: "";
+
+            if (!$stmt->execute()) {
+                die("Failed to execute query " . $stmt->error);
+            }//FIXME only show error during debugging
+            //printf("%d Row inserted with ID %d.\n", $stmt->affected_rows, $conn->insert_id);
+            $stmt->close();
+
+        }
+    }
+}
+if(!isset($_FILES['userfile'])) {
+    echo '<p>Please select a file</p>';
+} else {
+    upload();
+    ?>
+    <p><?php echo "Hello ".$_POST["name"].", "?></p>
+    <p><?php echo "We have recieved your application for the  ".$_SESSION['occupation']." position at ".$_SESSION['company']." in ".$_SESSION['location']."."?></p>
+    <p><?php echo "You have also received a confirmation email.//TO DO//"?></p>
+    <form action="joblistings.php" method="get">
+
+        <button name="subject" type="submit" value="login">Back to home page</button>
+
+    </form>
+    <?php
+}
+
+?>
+
+</body>
+</html>
