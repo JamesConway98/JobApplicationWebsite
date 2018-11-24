d89a3cec8f853a06a280375890dad84f2d4df7c2
Valts Blezurs <cpb16163@uni.strath.ac.uk>
2018-11-24 21:06:33 +0200

    basic search box

--- joblistings.php
+++ joblistings.php
@@ -13,44 +13,37 @@
 </header>
 <body>
 <div class="col">
+    <input type="text" id="searchBox" onkeyup="loadDoc()" placeholder="Search for job.." title="Type in a job">
+    <div id="table">
 <?php
-//require 'navbar.php';
-require 'connectdb.php';
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    if (isset($_GET["page"]) && !($_GET["page"] < 1)) {
-        $page = $_GET["page"];
-    } else {
-        $page = 1;
-    }
-    $page_size = 15;
-    $start_from = ($page - 1) * $page_size;
-
-    $sql = 'SELECT Job.ID, Job.Title, Job.Company, Job.Deadline, Job.Location, `occ`.`Occupation`, `typ`.`Type` FROM `Job` LEFT OUTER JOIN Occupation occ ON Job.Occupation_ID = occ.ID LEFT OUTER JOIN `Type` typ ON `Job`.`Type_ID` = `typ`.`ID` ORDER BY Deadline ASC, Title ASC LIMIT ' . $start_from . ', ' . $page_size;
-    $jobs = $conn->query($sql);
-    if (isset($jobs->num_rows) && $jobs->num_rows > 0) {
-        echo '<table>';
-        echo "<tr><th>Title</th><th>Company</th><th>Deadline</th><th>Location</th><th>Occupation</th><th>Type</th><th></th></tr>";
-        while ($row = $jobs->fetch_assoc()) {
-            echo "<tr><td>" . $row["Title"] . "</td><td>" . $row["Company"] . "</td><td>" . $row["Deadline"] . "</td><td>" . $row["Location"] . "</td><td>" . $row["Occupation"] . "</td><td>" . $row["Type"] . "</td><td><form action='details.php' method='get'><button name='more' type='submit' value='" . $row["ID"] . "'>+</button> </form></td></tr>";
-        }
-        echo "</table>";
-        echo "<div>";
-        if ($page - 1 > 0) {
-            echo "<a href='joblistings.php?page=" . ($page - 1) . "'><</a>  ";
-        }
-        $sql = "SELECT COUNT(ID) as `count` FROM `Job`";
-        $num_rows = $conn->query($sql)->fetch_assoc()["count"];
-        if ($page + 1 <= ceil($num_rows / $page_size)) {
-            echo "<a href='joblistings.php?page=" . ($page + 1) . "'>></a>";
-        }
-        echo "</div>";
-    } else {
-        echo '<p>No job listings available.</p>';
-    }
-}
+require 'jobtable.php';
 ?>
+    </div>
 </div>
+
+<script>
+    function loadDoc() {
+        var xhttp;
+        var input = document.getElementById("searchBox").value.trim();
+        if (window.XMLHttpRequest) {
+            // code for modern browsers
+            xhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("table").innerHTML = this.responseText;
+            }
+        };
+        if(input.length !== 0)
+            xhttp.open("GET", "jobtable.php?search=" + input, true);
+        else
+            xhttp.open("GET", "jobtable.php", true);
+        xhttp.send();
+    }
+</script>
+
 </body>
 </html>
---
+++ jobtable.php
@@ -0,0 +1,41 @@
+<?php
+require 'connectdb.php';
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    if (isset($_GET["page"]) && !($_GET["page"] < 1)) {
+        $page = $_GET["page"];
+    } else {
+        $page = 1;
+    }
+    $page_size = 15;
+    $start_from = ($page - 1) * $page_size;
+
+    $condition = "";
+    if(isset($_GET["search"])){
+        $condition = " WHERE Job.Title LIKE \"%" . $_GET["search"] . "%\"";
+    }
+
+    $sql = 'SELECT Job.ID, Job.Title, Job.Company, Job.Deadline, Job.Location, `occ`.`Occupation`, `typ`.`Type` FROM `Job` LEFT OUTER JOIN Occupation occ ON Job.Occupation_ID = occ.ID LEFT OUTER JOIN `Type` typ ON `Job`.`Type_ID` = `typ`.`ID` ' . $condition . ' ORDER BY Deadline ASC, Title ASC, Job.ID ASC LIMIT ' . $start_from . ', ' . $page_size;
+    $jobs = $conn->query($sql);
+    if (isset($jobs->num_rows) && $jobs->num_rows > 0) {
+        echo '<table id="table">';
+        echo "<tr><th>Title</th><th>Company</th><th>Deadline</th><th>Location</th><th>Occupation</th><th>Type</th><th></th></tr>";
+        while ($row = $jobs->fetch_assoc()) {
+            echo "<tr><td>" . $row["Title"] . "</td><td>" . $row["Company"] . "</td><td>" . $row["Deadline"] . "</td><td>" . $row["Location"] . "</td><td>" . $row["Occupation"] . "</td><td>" . $row["Type"] . "</td><td><form action='details.php' method='get'><button name='more' type='submit' value='" . $row["ID"] . "'>+</button> </form></td></tr>";
+        }
+        echo "</table>";
+        echo "<div>";
+        if ($page - 1 > 0) {
+            echo "<a href='joblistings.php?page=" . ($page - 1) . "'><</a>  ";
+        }
+        $sql = "SELECT COUNT(ID) as `count` FROM `Job` " . $condition;
+        $num_rows = $conn->query($sql)->fetch_assoc()["count"];
+        if ($page + 1 <= ceil($num_rows / $page_size)) {
+            echo "<a href='joblistings.php?page=" . ($page + 1) . "'>></a>";
+        }
+        echo "</div>";
+    } else {
+        echo '<p>No job listings available.</p>';
+    }
+}
