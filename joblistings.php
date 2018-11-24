f18ddb75662adb89ed6e49e1e571ba3f44756dd4
Valts Blezurs <cpb16163@uni.strath.ac.uk>
2018-11-24 23:57:38 +0200

    more filters

--- joblistings.php
+++ joblistings.php
@@ -14,7 +14,71 @@
 <body>
 <div class="col">
     <input type="text" id="searchBox" onkeyup="loadDoc('')" placeholder="Search for job.." title="Type in a job">
-    <input type="checkbox" id="expired" value="exp" checked> don't show expired listings<br>
+    <input type="checkbox" id="expired" onclick="loadDoc('')" checked> don't show expired listings<br>
+    Occupation:
+    <select id="occ" onchange="loadDoc('')">
+        <option value="">Select</option>
+        <?php
+        require 'connectdb.php';
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } else {
+            $sql = "SELECT * FROM Occupation ORDER BY Occupation ASC";
+            $occ = $conn->query($sql);
+            if (isset($occ->num_rows) && $occ->num_rows > 0)
+                while ($row = $occ->fetch_assoc())
+                    echo '<option value="' . $row["Occupation"] . '">' . $row["Occupation"] . '</option>';
+        }
+        ?>
+    </select>
+    Location:
+    <select id="loc" onchange="loadDoc('')">
+        <option value="">Select</option>
+        <?php
+        require 'connectdb.php';
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } else {
+            $sql = "SELECT DISTINCT Location FROM Job ORDER BY Location ASC";
+            $loc = $conn->query($sql);
+            if (isset($loc->num_rows) && $loc->num_rows > 0)
+                while ($row = $loc->fetch_assoc())
+                    echo '<option value="' . $row["Location"] . '">' . $row["Location"] . '</option>';
+        }
+        ?>
+    </select>
+    Company:
+    <select id="comp" onchange="loadDoc('')">
+        <option value="">Select</option>
+        <?php
+        require 'connectdb.php';
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } else {
+            $sql = "SELECT DISTINCT Company FROM Job ORDER BY Company ASC";
+            $comp = $conn->query($sql);
+            if (isset($comp->num_rows) && $comp->num_rows > 0)
+                while ($row = $comp->fetch_assoc())
+                    echo '<option value="' . $row["Company"] . '">' . $row["Company"] . '</option>';
+        }
+        ?>
+    </select>
+    Type:
+    <select id="type" onchange="loadDoc('')">
+        <option value="">Select</option>
+        <?php
+        require 'connectdb.php';
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } else {
+            $sql = "SELECT * FROM `Type` ORDER BY `Type`.`Type` ASC";
+            $type = $conn->query($sql);
+            if (isset($type->num_rows) && $type->num_rows > 0)
+                while ($row = $type->fetch_assoc())
+                    echo '<option value="' . $row["Type"] . '">' . $row["Type"] . '</option>';
+        }
+        ?>
+    </select>
     <div id="table">
         <?php
         require 'jobtable.php';
@@ -25,7 +89,14 @@
 <script>
     function loadDoc(page) {
         var xhttp;
+        var args = "";
         var input = document.getElementById("searchBox").value.trim();
+        var exp = document.getElementById("expired").checked.toString();
+        var occ = document.getElementById("occ").value;
+        var loc = document.getElementById("loc").value;
+        var comp = document.getElementById("comp").value;
+        var type = document.getElementById("type").value;
+
         if (window.XMLHttpRequest) {
             // code for modern browsers
             xhttp = new XMLHttpRequest();
@@ -38,12 +109,20 @@
                 document.getElementById("table").innerHTML = this.responseText;
             }
         };
-        var exp = document.getElementById("expired").checked.toString();
         if (input.length !== 0)
-            input = "&search=" + input;
+            args += "&search=" + input;
         if (page.length !== 0)
-            page = "&page=" + page;
-        xhttp.open("GET", "jobtable.php?exp=" + exp + input + page, true);
+            args += "&page=" + page;
+        if (occ.length !== 0)
+            args += "&occ=" + occ;
+        if (loc.length !== 0)
+            args += "&loc=" + loc;
+        if (comp.length !== 0)
+            args += "&comp=" + comp;
+        if (type.length !== 0)
+            args += "&type=" + type;
+
+        xhttp.open("GET", "jobtable.php?exp=" + exp + args, true);
         xhttp.send();
     }
 </script>
--- jobtable.php
+++ jobtable.php
@@ -15,14 +15,35 @@ if ($conn->connect_error) {
     if (!isset($_GET["exp"]) || $_GET["exp"] === "true")
         $condition = " WHERE Job.Deadline >= " . date("Ymd");
     if (isset($_GET["search"]))
-        if(strlen($condition) > 0)
+        if (strlen($condition) > 0)
             $condition .= " AND Job.Title LIKE \"%" . $_GET["search"] . "%\"";
         else
             $condition = " WHERE Job.Title LIKE \"%" . $_GET["search"] . "%\"";
+    if (isset($_GET["occ"]))
+        if (strlen($condition) > 0)
+            $condition .= " AND occ.Occupation = \"" . $_GET["occ"] . "\"";
+        else
+            $condition = " WHERE occ.Occupation = \"" . $_GET["occ"] . "\"";
+    if (isset($_GET["loc"]))
+        if (strlen($condition) > 0)
+            $condition .= " AND Job.Location = \"" . $_GET["loc"] . "\"";
+        else
+            $condition = " WHERE Job.Location = \"" . $_GET["loc"] . "\"";
+    if (isset($_GET["comp"]))
+        if (strlen($condition) > 0)
+            $condition .= " AND Job.Company = \"" . $_GET["comp"] . "\"";
+        else
+            $condition = " WHERE Job.Company = \"" . $_GET["comp"] . "\"";
+    if (isset($_GET["type"]))
+        if (strlen($condition) > 0)
+            $condition .= " AND typ.Type = \"" . $_GET["type"] . "\"";
+        else
+            $condition = " WHERE typ.Type = \"" . $_GET["type"] . "\"";
+
 
     $sql = 'SELECT Job.ID, Job.Title, Job.Company, Job.Deadline, Job.Location, `occ`.`Occupation`, `typ`.`Type` FROM `Job` LEFT OUTER JOIN Occupation occ ON Job.Occupation_ID = occ.ID LEFT OUTER JOIN `Type` typ ON `Job`.`Type_ID` = `typ`.`ID` ' . $condition . ' ORDER BY Deadline ASC, Title ASC, Job.ID ASC LIMIT ' . $start_from . ', ' . $page_size;
     $jobs = $conn->query($sql);
-    if (isset($jobs->num_rows) && $jobs->num_rows > 0) {
+    if (isset($jobs->num_rows) && $jobs->num_rows > 0) {$num_rows = $jobs->num_rows;
         echo '<table id="table">';
         echo "<tr><th>Title</th><th>Company</th><th>Deadline</th><th>Location</th><th>Occupation</th><th>Type</th><th></th></tr>";
         while ($row = $jobs->fetch_assoc()) {
@@ -33,8 +54,10 @@ if ($conn->connect_error) {
         if ($page - 1 > 0) {
             echo "<button type='button' onclick='loadDoc(" . ($page - 1) . ")'><</button>  ";
         }
-        $sql = "SELECT COUNT(ID) as `count` FROM `Job` " . $condition;
-        $num_rows = $conn->query($sql)->fetch_assoc()["count"];
+
+        $sql = 'SELECT Job.ID FROM `Job` LEFT OUTER JOIN Occupation occ ON Job.Occupation_ID = occ.ID LEFT OUTER JOIN `Type` typ ON `Job`.`Type_ID` = `typ`.`ID` ' . $condition . ' ORDER BY Deadline ASC';
+        $num_rows = $conn->query($sql)->num_rows;
+
         if ($page + 1 <= ceil($num_rows / $page_size)) {
             echo "<button type='button' onclick='loadDoc(" . ($page + 1) . ")'>></button>";
         }
